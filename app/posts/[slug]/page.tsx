62a8630b84ec40aab03c305920ce73184943d9b0
seul-dev <seul.dev@gmail.com>
2023-09-11 17:07:48 +0900

    feat: show posts

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { format, parseISO } from 'date-fns';
+import { allPosts } from 'contentlayer/generated';
+import { Mdx } from '@/components/MDXComponents';
+
+export const generateStaticParams = async () =>
+  allPosts.map((post) => ({ slug: post._raw.flattenedPath }));
+
+export const generateMetadata = ({ params }: { params: { slug: string } }) => {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+  return { title: post.title };
+};
+
+export default function PostLayout({ params }: { params: { slug: string } }) {
+  const post = allPosts.find((post) => post._raw.flattenedPath === params.slug);
+  if (!post) throw new Error(`Post not found for slug: ${params.slug}`);
+
+  return (
+    <article className='mx-auto max-w-xl py-8'>
+      <div className='mb-8 text-center'>
+        <time dateTime={post.date} className='mb-1 text-xs text-gray-600'>
+          {format(parseISO(post.date), 'LLLL d, yyyy')}
+        </time>
+        <h1 className='text-3xl font-bold'>{post.title}</h1>
+      </div>
+      <Mdx code={post.body.code} />
+    </article>
+  );
+}
